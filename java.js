550f405cc5fd503be6c079ef992e680074b883b1
ulugbekAbdullayev <ulu23503@gmail.com>
2024-10-15 23:36:48 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,178 @@
+
+
+
+//if 4
+// let a = +prompt('1-son')
+// let b = +prompt('2-son')
+// let c = +prompt('3-son')
+// let musbat=0
+
+// if (a > 0) {
+//   musbat++
+// } 
+// if (b > 0) {
+//   musbat++
+// }
+// if (c > 0) {
+//     musbat++}
+// console.log('musbat sonlar - '+musbat);
+
+
+
+//if 5
+// let son1 = +prompt("birinchi son kiriting ")
+// let son2 = +prompt("ikkinchi son kiriting ")
+// let son3 = +prompt("uchinchi son kiriting ")
+// let musbat_sanoq=0
+// let manfiy_sanoq=0
+// let nol_sanoq=0
+// if (son1 > 0) {
+//   musbat_sanoq++
+// } else if (son1 < 0) {
+//   manfiy_sanoq++
+// } else {
+//   nol_sanoq++
+// }
+// if (son2 > 0) {
+//   musbat_sanoq++
+// } else if (son2 < 0) {
+//   manfiy_sanoq++
+// } else {
+//   nol_sanoq++
+// }
+// if (son3 > 0) {musbat_sanoq++}
+// else if (son3 < 0) {manfiy_sanoq++}
+// else {nol_sanoq++}
+// console.log('musbat '+musbat_sanoq);
+// console.log('manfiy '+manfiy_sanoq);
+// console.log('nol '+nol_sanoq);
+
+
+//if 6
+// let a=+prompt('1-son')
+// let b=+prompt('2-son')
+// let c
+// if (a>b){
+//     c=a
+// }
+// if (b>a){
+//     c=b
+// }
+// console.log('katta raqam :'+c);
+
+
+
+//if 7
+// let a = +prompt('a')
+// let b = +prompt('b')
+// let kichikSon;
+// let tartibRaqami;
+// if (a < b) {
+//     kichikSon = a;
+//     tartibRaqami = 1; 
+// } else if (b < a) {
+//     kichikSon = b;
+//     tartibRaqami = 2;
+// } 
+// console.log('kichik son:'+kichikSon);
+// console.log('tartib raqami:'+tartibRaqami);
+
+
+
+
+
+//if 8
+// let a=+prompt("a")
+// let b=+prompt("b")
+
+// if (a>b){
+//     console.log('katta son '+a)
+//     console.log('kichik son '+b);
+    
+// }else if (b>a){
+//     console.log('katta son '+b)
+//     console.log('kichik son '+a);
+// }else {console.log('bular teng '+a);
+// }
+
+
+
+
+//if 9
+// let a=+prompt("a")
+// let b=+prompt("b")
+// let c
+// if (a>=b){
+// c=a
+// a=b 
+// b=c 
+// }
+// console.log('a soni '+a);
+// console.log('b soni '+b);
+
+
+//if 10
+// let a=+prompt('a')
+// let b=+prompt('b')
+// let y 
+// let nol
+// if (a>b){
+//     y=a+b
+// }else if (b>a){
+//     y=b+a;
+// }if (a=b){
+// nol=a%b
+
+// }
+// console.log('yigindi ' +y);
+// console.log('bir xil son  '+nol);
+
+
+
+
+//if 3    qo'shimcha++++++++++++++
+// let a=+prompt('son kirit')
+// if (a>0){
+//     a++
+// }
+// else if (a<0){
+//     a=a-2
+// }else{
+//     a=10
+// }
+// console.log(a);
+
+
+
+
+
+//qo'shimcha+++++++++
+
+// let oy = +prompt("oyni kiriting")
+// if(oy == 1){
+//     console.log("yanvar");
+// }else if(oy == 2){
+//     console.log("fevral");
+// }else if(oy == 3){
+//     console.log("mart");
+// }else if(oy == 4){
+//     console.log("aprel");
+// }else if(oy == 5){
+//     console.log("may");
+// }else if(oy == 6){
+//     console.log("iyun");
+// }else if(oy == 7){
+//     console.log("iyul");
+// }else if(oy == 8){
+//     console.log("avgust");
+// }else if(oy == 9){
+//     console.log("sentabr");
+// }else if(oy == 10){
+//     console.log("oktabr");
+// }else if(oy == 11){
+//     console.log("noyabr");
+// }else if(oy == 12){
+//     console.log("dekabr");
+// }else{
+//     console.log("bunday oy yoq");
+// }
